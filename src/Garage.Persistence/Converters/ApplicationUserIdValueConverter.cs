05d8662d39d3d6502d2ab51ebbea2adbaf2e30e3
Jeremy Knight <492328+jeremyknight-me@users.noreply.github.com>
2024-03-16 18:21:52 -0500

    removed unused class and renamed UserIdValueConverter

--- src/Garage.Persistence/Converters/ApplicationUserIdValueConverter.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-
-namespace Garage.Persistence.Converters;
-
-internal sealed class ApplicationUserIdValueConverter(ConverterMappingHints? mappingHints = null)
-    : ValueConverter<ApplicationUserId, string>(id => id.Id, value => new ApplicationUserId(value), mappingHints)
-{
-    public ApplicationUserIdValueConverter() : this(null) { }
-}
---
+++ src/Garage.Persistence/Converters/UserIdValueConverter.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace Garage.Persistence.Converters;
+
+internal sealed class UserIdValueConverter(ConverterMappingHints? mappingHints = null)
+    : ValueConverter<UserId, string>(id => id.Value, value => new UserId(value), mappingHints)
+{
+    public UserIdValueConverter() : this(null) { }
+}
